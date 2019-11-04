4cfd5ad39d63c99f3db9a5d32fb42e69a3c39c46
Dave Lee <davelee@Daves-MacBook-Pro.local>
2019-11-04 15:33:29 +0100

    Delete files

--- TimNeidig_problem3.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-read -p "Bitte geben Sie einen Dateinamen ein: " DNAME   # liest nach ./TimNeidig_problem3.sh die Variable VNAME
-
-avconv -i  $DNAME -vsync 1 -r 50 out-%03d.jpg            # s.3.a) convert a video of your choice to a sequence of images
-
-convert -delay 20 -loop 1 out-*.jpg animated.gif         # s.3.b) convert this sequence of images to an animated gif
-
-rm *.jpg
